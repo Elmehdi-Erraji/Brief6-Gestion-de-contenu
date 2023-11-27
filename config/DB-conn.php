a1f5a2781029d3cadb3d1c20fca6cdbb879586f3
Mehdi <elmahdi20erraji@gmail.com>
2023-11-27 09:02:53 +0100

    config file update

--- config/DB-conn.php
+++ config/DB-conn.php
@@ -1,14 +1,14 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "brief6";
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "brief6";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-// echo "Connected successfully";
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  // echo "Connected successfully";
